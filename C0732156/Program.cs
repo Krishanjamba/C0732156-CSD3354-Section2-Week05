32adba27d1fa8b091195b59d005631b5de60543b
visualstudio <visualstudio@122-PC04>
2019-02-06 10:07:58 -0500

    commit11

--- C0732156/Program.cs
+++ C0732156/Program.cs
@@ -4,9 +4,28 @@ namespace C0732156
 {
     class Program
     {
+        static int i = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MethodA();
+        }
+
+            public static void MethodA()
+
+        {
+            while (Peanut())
+            {
+                Console.WriteLine(i);
+                if (i>10)
+                {
+                    return;
+                }
+            }
+        }
+        public static bool Peanut()
+        {
+            i++;
+            return true;
         }
     }
 }
